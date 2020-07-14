d808be693f0331e68822affbf24d509c1ecd01fd
Marcus Moreno <legendary.1198@gmail.com>
2020-07-14 00:14:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,18 @@
 def prime?(num)
+  
   start = 2
+  
   if num > 1
+    
     test = (start..num-1).to_a
+    
     test.none? { |test_num|
-      num % test_num == 0
-    }
+    
+      num % test_num == 0}
   else
+    
+    
     false
+    
   end
 end 
